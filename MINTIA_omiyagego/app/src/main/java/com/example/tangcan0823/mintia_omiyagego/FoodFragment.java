367d598cd8bdd2694b6437655a68fec76a7b92c5
YRZhong <zhongyuran@gmail.com>
2016-08-28 01:07:15 +0900

    demo

--- MINTIA_omiyagego/app/src/main/java/com/example/tangcan0823/mintia_omiyagego/FoodFragment.java
+++ MINTIA_omiyagego/app/src/main/java/com/example/tangcan0823/mintia_omiyagego/FoodFragment.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
  * Created by tangcan0823 on 2016/08/25.
  */
 public class FoodFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
