1059177b54503c292bf6f3a15ad6157efc42e262
puyue2016 <s1620722@u.tsukuba.ac.jp>
2016-08-29 09:52:43 +0900

    8-29
    skip機能をつけって、画面サイズを調整

--- MINTIA_omiyagego/app/src/main/java/com/example/tangcan0823/mintia_omiyagego/VideoControl.java
+++ MINTIA_omiyagego/app/src/main/java/com/example/tangcan0823/mintia_omiyagego/VideoControl.java
@@ -7,15 +7,22 @@ import android.os.Message;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainControl extends AppCompatActivity {
+public class VideoControl extends AppCompatActivity {
     private Thread1 th1;//Declaration thread
     private Thread2 th2;
     private Handler handler1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_control);
+        //取消标题
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //取消状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_video_control);
         th1 = new Thread1();
         th1.setName("T1");//give it name
         th1.start();
@@ -43,8 +50,8 @@ public class MainControl extends AppCompatActivity {
             Looper.prepare();
             handler1 = new Handler(){
                 public void handleMessage(android.os.Message msg) {    //if Thread1 get the message from Thread2
-                    SystemClock.sleep(4000);
-                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);//open the page
+                    SystemClock.sleep(17000);
+                    Intent intent = new Intent(getApplicationContext(),MainControl.class);//open the page
                     startActivity(intent);
                 };
             };
@@ -53,8 +60,6 @@ public class MainControl extends AppCompatActivity {
         }
 
     }
-
-
     public class Thread2 extends Thread{
         @Override
         public void run() {
@@ -62,11 +67,9 @@ public class MainControl extends AppCompatActivity {
             msg.what = 1;
             msg.obj = System.currentTimeMillis()+"";
             handler1.sendMessage(msg);
-            Intent intent = new Intent(getApplicationContext(),start.class);
+            Intent intent = new Intent(getApplicationContext(),video.class);
             startActivity(intent);
 
         }
     }
-
-
 }
---
+++ MINTIA_omiyagego/app/src/main/java/com/example/tangcan0823/mintia_omiyagego/video.java
@@ -0,0 +1,56 @@
+package com.example.tangcan0823.mintia_omiyagego;
+
+import android.content.Intent;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import java.io.File;
+
+public class video extends AppCompatActivity {
+    private VideoView videoView = null;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //取消标题
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //取消状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_video);
+        init();
+        videoView.setVideoPath(Environment.getExternalStorageDirectory() + File.separator + "my.mp4");
+        videoView.requestFocus();
+        videoView.start();
+
+
+    }
+    /*@Override
+    public void onPause(){
+        finish();
+    }
+
+    @Override
+    public void onStop(){
+        video.finish();
+    }*/
+    private void init() {
+         videoView = (VideoView) findViewById(R.id.videoView);
+    }
+    public void OnMySelfClick(View v)
+    {
+       finish();
+       Intent intent = new Intent(getApplicationContext(),MainControl.class);//open the page
+        startActivity(intent);
+    }
+
+
+}
